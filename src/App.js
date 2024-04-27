515cb5350767f390594832b092631048fd93e917
Sunny1606 <solescobares@gmail.com>
2024-04-26 23:57:03 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import home from "./components/home/home";
+import Appcss from "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="conteiner">
+      <input className="input"></input>
+
+    <div className="button-conteiner">
+    <button className="create-button">CREAR TAREA</button>
+      <button className="delete-button">ELIMINAR</button>
+    </div>
+      
+
     </div>
   );
 }
